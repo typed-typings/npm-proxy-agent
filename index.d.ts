0b43a1ce0c09da36f92fa236075993d214c0da15
Blake Embrey <hello@blakeembrey.com>
2016-01-30 12:13:23 -0800

    Add optional hostname

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 import { Agent } from 'http';
 
 declare class ProxyAgent extends Agent {
-  constructor (uri: string | { protocol: string; host: string; port?: string; proxies?: any });
+  constructor (uri: string | { protocol: string; hostname?: string; host?: string; port?: string; proxies?: any });
 
   static proxies: { [key: string]: typeof Agent };
 }
